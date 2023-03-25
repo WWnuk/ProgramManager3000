f4c2b434f71b3f8d2aecc7970f225d844a025fed
Wawrzyniec Wnuk <wawrzyniec.wnuk@gmail.com>
2023-03-25 19:58:43 +0100

    4th commit
    Edit rows work, error control for edit in YAML works, Add new program feature need to be done

--- main.py
+++ main.py
@@ -5,27 +5,115 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
 from kivy.properties import NumericProperty
 import sqlite3
 import yaml
+from yaml.loader import SafeLoader
+from functools import partial
+
+
+
+class InvalidIdValueException(Exception):
+    def __init__(self):
+            super().__init__("You can`t modify 'id' value!")
+class InvalidDownloadedValueException(Exception):
+    def __init__(self):
+            super().__init__("You can`t modify 'downloaded' value!")
+class InvalidInstalledValueException(Exception):
+    def __init__(self):
+            super().__init__("You can`t modify 'installed' value!")      
+
+
+
 Builder.load_file('.\gui.kv')
+
+
+class PopupWindowAdd(Popup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.textinput_id.text = """
+        name:Name of a program
+        url:URL link to program
+        install_command:Program will be installed using this command
+        """
+
+
+
+
+
+
+
+
+
+class PopupWindowEdit(Popup):
+    def __init__(self, row_id, **kwargs):
+        super().__init__(**kwargs)
+        self.text_from_row_label = MainScreen.all_textinputs[row_id].text
+        self.ids.textinput_id.text = self.text_from_row_label
+    def save_changes_to_database(self):
+        try:
+            self.edited_text_in_yaml = yaml.load(self.ids.textinput_id.text, Loader = SafeLoader)
+            self.orginal_text_in_yaml = yaml.load(self.text_from_row_label, Loader = SafeLoader)
+            if self.edited_text_in_yaml['id'] != self.orginal_text_in_yaml['id']:
+                raise InvalidIdValueException()
+
+            if self.edited_text_in_yaml['downloaded'] != self.orginal_text_in_yaml['downloaded']:
+                raise InvalidDownloadedValueException()
+
+            if self.edited_text_in_yaml['installed'] != self.orginal_text_in_yaml['installed']:
+                raise InvalidInstalledValueException()
+
+            sqlite_connection = sqlite3.connect('.\programs.db')
+            cursor = sqlite_connection.cursor()
+            cursor.execute("""UPDATE ProgramManager3000 SET name=?,url=?,install_command=?,downloaded=?,installed=? WHERE id=?""",
+                         (self.edited_text_in_yaml['name'], self.edited_text_in_yaml['url'], self.edited_text_in_yaml['install_command'],
+                             self.edited_text_in_yaml['downloaded'], self.edited_text_in_yaml['installed'], self.edited_text_in_yaml['id']))
+            sqlite_connection.commit()
+            cursor.close()
+            
+        except yaml.YAMLError as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = 'Error in YAML syntax. Changes are not saved to database.'))
+            error_popup.open()
+        except KeyError as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = 'You modifed KEY. Please don`t do that.'))
+            error_popup.open()
+        except sqlite3.Error as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = str(error)))
+            error_popup.open()
+        except InvalidInstalledValueException as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = "You can`t modify 'installed' value!"))
+            error_popup.open()
+        except InvalidDownloadedValueException as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = "You can`t modify 'downloaded' value!"))
+            error_popup.open()
+        except InvalidIdValueException as error:
+            error_popup = Popup(size_hint=(None, None), size=(800, 400), title = 'Error', content = Label(text = "You can`t modify 'id' value!"))
+            error_popup.open()
+
 
 # Declare both screens
 class MainScreen(Screen):
+    all_textinputs = []
+    all_checkboxes = []
+    all_buttons = []
     def __init__(self, **kw):
         super().__init__(**kw)
-        self.all_textinputs = []
-        self.all_checkboxes = []
-        self.all_buttons = []
+
+    def edit_row(self, *args, **kwargs):
+        print(kwargs['row_id'])
+        popup = PopupWindowEdit(kwargs['row_id'], size_hint=(None, None), size=(800, 400), title = 'Edit row in YAML')
+        popup.open()
 
 
-    def edit_row(self, instance):
-        print('click')
-        
+    def add_new_program(self):
+        popup = PopupWindowAdd(size_hint=(None, None), size=(800, 400), title = 'Add new program')
+        popup.open()
+
 
     def get_search_results_from_database(self):
         
@@ -62,14 +150,15 @@ class MainScreen(Screen):
                                   column_names[4]: column_data[4],
                                   column_names[5]: column_data[5]}
                 converted_to_yaml = yaml.dump(yaml_formatted, sort_keys = False)
+                rid= data_returned_from_query.index(column_data)
 
 
 
-                textinput = Label(font_size = '15sp', size_hint_x = 0.8, text = str(converted_to_yaml))
+                textinput = Label(font_size = '15sp', size_hint_x = 0.8, text = converted_to_yaml, text_size = (600, None))
                 checkbox = CheckBox(size_hint_x = 0.1)
                 button_in_box_layout = BoxLayout(orientation = 'vertical', size_hint_x = 0.1)
                 button_in_box_layout.add_widget(Label(text = ''))
-                button_in_box_layout.add_widget(Button(text = 'Edit row!', on_press = self.edit_row))
+                button_in_box_layout.add_widget(Button(text = 'Edit row!', on_press = partial(self.edit_row, row_id = rid)))
                 button_in_box_layout.add_widget(Label(text = ''))
 
 
